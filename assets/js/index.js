d9dda342629b758b419e19045153fa9b05af3cab
Philip Garip <phil.garip@gmail.com>
2021-05-13 11:26:41 -0400

    new reservation object start

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,21 @@
 //DEPENDENCIES========
     //BUTTONS
     //FORM/INPUTS
+        //name, phone, email, id
     //DIVS FOR HTML LISTS
 
 //OBJECT CONSTRUCTOR FOR RESERVATIONS
 
+
 //FUCNTIONS==========
     //CREATE RES OBJECT
         //FORM USER INPUT, INTO OBJECT PROPERTIES
+let newReservation = {
+    name: userName,
+    phone: userPhone,
+    email: userEmail,
+    ID: id
+}
 
     // FETCH DATA
         //
