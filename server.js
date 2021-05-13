4d68dff3ac6a90387ea423a902e1946ee91403aa
Mandell Butler <mandell.butler@outlook.com>
2021-05-13 12:29:01 -0400

    REstructured folders and link html route

--- server.js
+++ server.js
@@ -1,8 +1,33 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const app = express()
 const PORT = process.env.PORT || 3800;
+
+// sets up Express to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+//API ROUTES
+    //CRUD :)
+        //CREATE - POST RESERVATIONS
+            //FS
+app.get('/tables', (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/tables.html"));
+});
+
+app.get('/reservations', (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/reservations.html"));
+});
+
+        //READ - GET RESERVATION OBJECTS
+        //UPDATE
+        //DELETE
+//HTML ROUTES
+    //GET, RES.SENDFILE TABLES
+    //GET, RES.SENDFILE RESERVATIONS
 
 app.get('/', function (req, res) {
     res.send('hello world')
@@ -10,18 +35,9 @@ app.get('/', function (req, res) {
 
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 app.listen(PORT, () => console.log(`We chopping it up on PORT ${PORT}`));
 // IT'S A SERVER!
 
-//API ROUTES
-    //CRUD :)
-        //CREATE - POST RESERVATIONS
-            //FS
-        //READ - GET RESERVATION OBJECTS
-        //UPDATE
-        //DELETE
-//HTML ROUTES
-    //GET, RES.SENDFILE TABLES
-    //GET, RES.SENDFILE RESERVATIONS
+
